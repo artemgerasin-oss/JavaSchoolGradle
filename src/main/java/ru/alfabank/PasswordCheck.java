7881326ec8e6cd84ab3c3fa97a3fd4c8802e0064
Work <artem_gerasin@mail.ru>
2026-04-28 15:29:10 +0400

    доделал проверку пароля.

--- src/main/java/ru/alfabank/PasswordCheck.java
+++ src/main/java/ru/alfabank/PasswordCheck.java
@@ -1,5 +1,7 @@
 package ru.alfabank;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 
@@ -10,6 +12,7 @@ public class PasswordCheck {
 
         String[] arrPassword = {"correctPassword", "1345z", "qwerty123"};
 
+        //int minlengthpass = 8;
         String target_1 = "1";
         String target_z = "z";
         String target_1234 = "1234";
@@ -24,6 +27,29 @@ public class PasswordCheck {
 
         for (int i=0; i < arrPassword.length; i++){
 
+            if(arrPassword[i].startsWith("1")){
+                //System.out.println("Пароль " + arrPassword[i] + " содержит 1 в начале");
+                found_1 = true;
+            }
+
+            if(arrPassword[i].endsWith("z")){
+                //System.out.println("Пароль" + arrPassword[i] + " содержит z в конце!");
+                found_z = true;
+            }
+
+            if(arrPassword[i].contains("123")){
+                //System.out.println("Пароль" + arrPassword[i] + " содержит 123 !");
+                found_1234 = true;
+            }
+
+            if(arrPassword[i].contains("qwerty")){
+                //System.out.println("Пароль" + arrPassword[i] + " содержит z в конце!");
+                found_qwety = true;
+            }
+            if (found_1234 || found_1 || found_z || found_qwety){
+                System.out.println("Пароль " + arrPassword[i] + " содержит недопустимые символы");
+                continue;
+            }else {System.out.println("Пароль " + arrPassword[i] + " корректный!");}
 
 
         }
